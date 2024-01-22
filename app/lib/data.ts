e45e9c9472d1db04eb1798f234b3d2581388ca5b
Rusland420 <rzfromyrizk@gmai.com>
2024-01-22 21:47:46 +0300

    Handling Errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,6 +11,7 @@ import {
 import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
+
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
